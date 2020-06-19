2330e14a258aacdb45872cdc6e8db9c4680985f4
rockshang <s2265681@163.com>
2020-06-19 16:08:29 +0800

    build

---
+++ build/precache-manifest.7a70ffa48f14b784611c197a2eaa0b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5314031282720912b386951ee9abdc82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffdc0ce32fb2228b6d9c",
+    "url": "./static/css/2.0eb214dc.chunk.css"
+  },
+  {
+    "revision": "027038532f919aef733d",
+    "url": "./static/css/main.48ad18f7.chunk.css"
+  },
+  {
+    "revision": "ffdc0ce32fb2228b6d9c",
+    "url": "./static/js/2.c90e5146.chunk.js"
+  },
+  {
+    "revision": "6bd09671d7bf778b9ee3f747bf38e80c",
+    "url": "./static/js/2.c90e5146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027038532f919aef733d",
+    "url": "./static/js/main.b1d46286.chunk.js"
+  },
+  {
+    "revision": "30ee5de7b1cbef37a821",
+    "url": "./static/js/runtime-main.2901a40e.js"
+  }
+]);
